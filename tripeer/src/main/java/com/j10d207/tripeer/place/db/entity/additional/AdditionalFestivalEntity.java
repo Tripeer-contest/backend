a7556f7212a8de7942bdbf35eb44e206e7ef6514
손동천 <kij8025@naver.com>
2024-09-02 00:35:40 +0900

    fix : festival entity 변수명 매핑 오류 수정

--- tripeer/src/main/java/com/j10d207/tripeer/place/db/entity/additional/AdditionalFestivalEntity.java
+++ tripeer/src/main/java/com/j10d207/tripeer/place/db/entity/additional/AdditionalFestivalEntity.java
@@ -32,9 +32,13 @@ public class AdditionalFestivalEntity {
     private String festivalGrade;
     private String program;
     private String spendTimeFestival;
+    @Column(name = "sponsor_1")
     private String sponsor1;
+    @Column(name = "sponsor_1_tel")
     private String sponsor1Tel;
+    @Column(name = "sponsor_2")
     private String sponsor2;
+    @Column(name = "sponsor_2_tel")
     private String sponsor2Tel;
     private String subEvent;
     private String ageLimit;
