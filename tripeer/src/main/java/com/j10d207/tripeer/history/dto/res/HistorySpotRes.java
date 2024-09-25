6d6ad376879801ddf516e1883394e5f06c89a1e6
yang geonu <yanggeonu@yangui-MacBookPro.local>
2024-09-25 15:00:09 +0900

    fix : 히스토리 디테일 dto 에 평균 별점과 내가 리뷰작성했는지 추가

--- tripeer/src/main/java/com/j10d207/tripeer/history/dto/res/HistorySpotRes.java
+++ tripeer/src/main/java/com/j10d207/tripeer/history/dto/res/HistorySpotRes.java
@@ -1,7 +1,9 @@
 package com.j10d207.tripeer.history.dto.res;
 
 import com.j10d207.tripeer.place.db.ContentTypeEnum;
+import com.j10d207.tripeer.place.db.entity.SpotReviewEntity;
 import com.j10d207.tripeer.plan.db.entity.PlanDetailEntity;
+import com.j10d207.tripeer.user.db.entity.UserEntity;
 
 import lombok.Builder;
 import lombok.Getter;
@@ -10,25 +12,32 @@ import lombok.Getter;
 @Builder
 public class HistorySpotRes {
 	private long planDetailId;
+	private long spotId;
 	private String title;
 	private String contentType;
 	private String image;
 	private String address;
 	private Double latitude;
 	private Double longitude;
+	private Double starPointAvg;
+	private boolean isWriteReview;
 	private int day;
 	private int step;
 	private int cost;
 
-	public static HistorySpotRes from(PlanDetailEntity planDetailEntity) {
+	public static HistorySpotRes from(PlanDetailEntity planDetailEntity, UserEntity userEntity) {
 		return HistorySpotRes.builder()
 			.planDetailId(planDetailEntity.getPlanDetailId())
+			.spotId(planDetailEntity.getSpotInfo().getSpotInfoId())
 			.title(planDetailEntity.getSpotInfo().getTitle())
 			.contentType(ContentTypeEnum.getNameByCode(planDetailEntity.getSpotInfo().getContentTypeId()))
 			.image(planDetailEntity.getSpotInfo().getFirstImage())
 			.address(planDetailEntity.getSpotInfo().getAddr1())
 			.latitude(planDetailEntity.getSpotInfo().getLatitude())
 			.longitude(planDetailEntity.getSpotInfo().getLongitude())
+			.starPointAvg(planDetailEntity.getSpotInfo().getSpotReviewList().stream().mapToDouble(
+				SpotReviewEntity::getStarPoint).average().orElse(0.0))
+			.isWriteReview(planDetailEntity.getSpotInfo().getSpotReviewList().stream().map(SpotReviewEntity::getUser).anyMatch(userEntity::equals))
 			.day(planDetailEntity.getDay())
 			.step(planDetailEntity.getStep())
 			.cost(planDetailEntity.getCost())
