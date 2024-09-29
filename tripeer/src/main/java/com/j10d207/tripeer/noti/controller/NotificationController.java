d4e3db43cf1de477d664c4e7f8a257bc619e3cb9
kimhaechang <khc9812121@gmail.com>
2024-09-29 18:32:08 +0900

    fix: token 저장 enum String 전략 추가

--- tripeer/src/main/java/com/j10d207/tripeer/noti/controller/NotificationController.java
+++ tripeer/src/main/java/com/j10d207/tripeer/noti/controller/NotificationController.java
@@ -148,8 +148,7 @@ public class NotificationController {
 	public Response<Void> testDiarySave(
 		@AuthenticationPrincipal CustomOAuth2User user
 	) {
-		publisher.publish();
-		// testService.testDiaryNoti(user.getUserId());
+		testService.testDiaryNoti(user.getUserId());
 		return Response.of(
 			HttpStatus.OK,
 			null,
