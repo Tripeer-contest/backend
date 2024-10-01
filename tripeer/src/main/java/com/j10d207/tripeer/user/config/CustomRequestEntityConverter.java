86596772800f9b6aa5277e65f7464d40a22e8ea2
손동천 <kij8025@naver.com>
2024-10-01 19:23:06 +0900

    fix : 파일 추가

--- tripeer/src/main/java/com/j10d207/tripeer/user/config/CustomRequestEntityConverter.java
+++ tripeer/src/main/java/com/j10d207/tripeer/user/config/CustomRequestEntityConverter.java
@@ -38,8 +38,7 @@ public class CustomRequestEntityConverter implements Converter<OAuth2Authorizati
     }
 
     private final String APPLE_URL = "https://appleid.apple.com";
-//    private final String APPLE_KEY_PATH = "/app/AuthKey_XKN39QTTDC.p8";
-    private final String APPLE_KEY_PATH = "1";
+    private final String APPLE_KEY_PATH = "AuthKey_XKN39QTTDC.p8";
     private final String APPLE_CLIENT_ID = "tripeer.tripeer.com";
     private final String APPLE_TEAM_ID = "7RBWP4PK88";
     private final String APPLE_KEY_ID = "XKN39QTTDC";
